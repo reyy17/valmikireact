34873ae8bc2e868e317f0e92b3484e69fed729ff
reyy17 <92677819+reyy17@users.noreply.github.com>
2023-09-21 11:59:40 +0530

    Update headerPrimary.js

--- src/components/header/headerPrimary.js
+++ src/components/header/headerPrimary.js
@@ -1,18 +1,28 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
-import "../../components/header/headerPrimary.css";
 import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
 import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
+import { useNavigate } from 'react-router-dom';
+import "../../components/header/headerPrimary.css";
+
+const baseURL = "http://localhost:14440/ashram/api/auth/v1";
 
 function HeaderPrimary() {
+  const navigate = useNavigate();
+  
   const [isSignupModalOpen, setSignupModalOpen] = useState(false);
-  const [isLoginModalOpen, setLoginModalOpen] = useState(false); 
+  const [isLoginModalOpen, setLoginModalOpen] = useState(false);
+  const [isForgotPasswordModalOpen, setForgotPasswordModalOpen] = useState(false); 
   const [formData, setFormData] = useState({
-    username: "",
+    name: "",
     email: "",
     password: "",
     role: "",
   });
+
+  const navigateToForgotPassword = () => {
+    setForgotPasswordModalOpen(true);
+  };
 
   const openSignupModal = () => {
     setSignupModalOpen(true);
@@ -30,6 +40,10 @@ function HeaderPrimary() {
     setLoginModalOpen(false);
   };
 
+  const closeForgotPasswordModal = () => {
+    setForgotPasswordModalOpen(false);
+  };
+
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
@@ -37,19 +51,80 @@ function HeaderPrimary() {
 
   const handleSignup = (e) => {
     e.preventDefault();
-    // Handle form submission for signup, e.g., send data to the server
-    // add validation and API calls here
-    console.log(formData);
-    closeSignupModal();
+    fetch(`${baseURL}/signup`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          setFormData({
+            name: "",
+            email: "",
+            password: "",
+            role: "",
+          });
+          console.log("Signup successful");
+          closeSignupModal();
+        } else {
+          console.error("Signup failed");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
 
   const handleLogin = (e) => {
     e.preventDefault();
-    // Handle form submission for login, e.g., send data to the server
-    // add validation and API calls here
-    console.log(formData);
-    closeLoginModal();
+    fetch(`${baseURL}/signin`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: formData.email,
+        password: formData.password,
+      }),
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          console.log("Login successful");
+          closeLoginModal();
+        } else {
+          console.error("Login failed");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
+
+  const handleResetPassword = (e) => {
+    e.preventDefault();
+    fetch(`${baseURL}/forgot`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `email=${formData.email}`,
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          console.log("Password reset request sent successfully");
+          closeForgotPasswordModal();
+          
+        } else {
+          console.error("Password reset request failed");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
   return (
     <div className="headerPrimary">
       <div className="left part">
@@ -71,7 +146,6 @@ function HeaderPrimary() {
           Login
         </div>
 
-        {/* Login Modal */}
         <Modal
           isOpen={isLoginModalOpen}
           onRequestClose={closeLoginModal}
@@ -82,9 +156,17 @@ function HeaderPrimary() {
             <form onSubmit={handleLogin}>
               <input
                 type="text"
-                name="username"
-                placeholder="Username"
-                value={formData.username}
+                name="name"
+                placeholder="Name"
+                value={formData.name}
+                onChange={handleInputChange}
+                required
+              />
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={formData.email}
                 onChange={handleInputChange}
                 required
               />
@@ -96,6 +178,10 @@ function HeaderPrimary() {
                 onChange={handleInputChange}
                 required
               />
+              <button className="forgot-password button" onClick={navigateToForgotPassword}>
+  Forgot Password
+</button>
+
               <button type="submit">Login</button>
               <button onClick={closeLoginModal}>Close</button>
             </form>
@@ -116,9 +202,9 @@ function HeaderPrimary() {
             <form onSubmit={handleSignup}>
               <input
                 type="text"
-                name="username"
-                placeholder="Username"
-                value={formData.username}
+                name="name"
+                placeholder="Name"
+                value={formData.name}
                 onChange={handleInputChange}
                 required
               />
@@ -151,8 +237,31 @@ function HeaderPrimary() {
             </form>
           </div>
         </Modal>
+
+        <Modal
+          isOpen={isForgotPasswordModalOpen}
+          onRequestClose={closeForgotPasswordModal}
+          contentLabel="Forgot Password Modal"
+        >
+          <div className="forgot-password-popup">
+            <h2>Forgot Password</h2>
+            <form onSubmit={handleResetPassword}>
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleInputChange}
+                required
+              />
+              <button type="submit">Reset Password</button>
+              <button onClick={closeForgotPasswordModal}>Close</button>
+            </form>
+          </div>
+        </Modal>
       </div>
     </div>
   );
 }
+
 export default HeaderPrimary;
