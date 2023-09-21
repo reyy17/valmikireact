0f1aafb34b313c1f462247a41150486a3b34259c
reyy17 <92677819+reyy17@users.noreply.github.com>
2023-09-21 12:00:39 +0530

    Update Mainpage.js

--- src/components/Mainpage.js
+++ src/components/Mainpage.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom"; // Import BrowserRouter
 import Header from "./Header";
 import HomeContent from "./HomeContent";
 import HomeFooter from "./HomeFooter";
 
 function Mainpage() {
   return (
-    <div className="App">
-      <Header />
-      <HomeContent />
-      <HomeFooter />
-    </div>
+    <Router> {/* Wrap the Mainpage component with Router */}
+      <div className="App">
+        <Header />
+        <HomeContent />
+        <HomeFooter />
+      </div>
+    </Router>
   );
 }
 
